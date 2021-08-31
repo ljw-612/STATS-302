bf45d2b7e9265f6cf336d76e03bf7431a688c85e
ljw-612 <lijingwei58466809@outlook.com>
2021-08-31 20:46:45 +0800

    11

--- week1/__init__.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-theta_list = np.zeros((14,1), dtype=float)
-
-print(theta_list)
--- week1/week1_part1_final.py
+++ week1/week1_part1_final.py
@@ -15,7 +15,6 @@ def load_data(data_file, train_test_proportion):
     y_test = y[int(p * num_data):].reset_index(drop=True)
     y_train = np.array([y_train]).T
     y_test = np.array([y_test]).T
-
     # Divide the input set
     data.drop(data.columns[[-1]], axis=1, inplace=True)
     x = data
@@ -24,14 +23,37 @@ def load_data(data_file, train_test_proportion):
 
     return x_train, x_test, y_train, y_test, num_data
 
+# visualize MAE & MSE
+def visualization(MSE_values_train, MSE_values_test, MAE_values_train, MAE_values_test, num_iter, alpha):
+    plt.figure( figsize= (10, 10) )
+
+    plt.subplot(2, 1, 1)
+    plt.plot(MSE_values_train, '-r')
+    plt.xlabel('Number of Iterations')
+    plt.ylabel('MSE for training set')
+    plt.title('Learning rate = {}, # of iter. = {}'.format(alpha, num_iter), fontsize=24, y=1.1)
+    plt.grid()
+
+    plt.subplot(2, 1, 2)
+    plt.plot(MAE_values_train, '-b')
+    plt.xlabel('Number of Iterations')
+    plt.ylabel('MAE for training set')
+    plt.grid()
+
+    plt.show()
+    return
+
+# calculate MAE & MSE
 def error_calculation(theta_list, x_train, y_train, x_test, y_test, num_data_x_train, num_data_x_test):
     # Calculate the MSE for training data
     h_x_train = np.matmul(x_train, theta_list)
     MSE_x_train = sum( (h_x_train - y_train)**2 ) / (2 * num_data_x_train)
+    MAE_x_train = sum( abs(h_x_train - y_train) ) / (2 * num_data_x_train)
     # Calculate the MSE for testing data
     h_x_test = np.matmul(x_test, theta_list)
     MSE_x_test = sum( (h_x_test - y_test)**2 ) / (2 * num_data_x_test)
-    return MSE_x_train[0], MSE_x_test[0]
+    MAE_x_test = sum( abs(h_x_test - y_test) ) / (2 * num_data_x_test)
+    return MSE_x_train[0], MAE_x_train[0], MSE_x_test[0], MAE_x_test[0]
 
 def gradient_descent(x_train, x_test, y_train, y_test, num_data, num_iter=50, alpha=0.00001):
     # get how long is the data (row number)
@@ -46,7 +68,10 @@ def gradient_descent(x_train, x_test, y_train, y_test, num_data, num_iter=50, al
     x_test = x_test.T
 
     theta_list = np.zeros((len_x_train + 1, 1), dtype=float)
-    h = 0
+
+    MSE_values_train = []; MSE_values_test = []
+    MAE_values_train = []; MAE_values_test = []
+
     for i in range(num_iter):
         h = np.matmul(x_train, theta_list)
         for i in range(0, len_x_train + 1):
@@ -56,22 +81,29 @@ def gradient_descent(x_train, x_test, y_train, y_test, num_data, num_iter=50, al
                 m = np.matmul( np.array([x_train[:,i]]), (h - y_train))
                 theta_list[i] = theta_list[i] - alpha * (1 / num_data) * m[0][0]
 
-    MSE_x_train, MSE_x_test = error_calculation(theta_list=theta_list, x_train=x_train, y_train=y_train, x_test=x_test,
-                      y_test=y_test, num_data_x_train=num_data_x_train, num_data_x_test=num_data_x_test)
-    return theta_list, MSE_x_train, MSE_x_test
+        MSE_x_train, MAE_x_train, MSE_x_test, MAE_x_test = error_calculation(theta_list=theta_list,
+                            x_train=x_train, y_train=y_train, x_test=x_test,y_test=y_test, num_data_x_train=num_data_x_train, num_data_x_test=num_data_x_test)
+
+        MSE_values_train.append(MSE_x_train); MSE_values_test.append(MSE_x_test)
+        MAE_values_train.append(MAE_x_train); MAE_values_test.append(MAE_x_test)
+
+    return theta_list, MSE_values_train, MSE_values_test, MAE_values_train, MAE_values_test
 
 def run(num_iter, alpha, train_test_proportion):
     x_train, x_test, y_train, y_test, num_data = load_data(data_file='boston_house_prices-rev-norm.csv',
                                                            train_test_proportion=train_test_proportion)
-    theta_list, MSE_x_train, MSE_x_test = gradient_descent(x_train, x_test, y_train, y_test,
+    theta_list, MSE_values_train, MSE_values_test, MAE_values_train, MAE_values_test = gradient_descent(x_train, x_test, y_train, y_test,
                                                            num_data=num_data, num_iter=num_iter, alpha=alpha)
-    return theta_list, MSE_x_train, MSE_x_test
+
+    visualization(MSE_values_train=MSE_values_train, MSE_values_test=MSE_values_test,
+                  MAE_values_train=MAE_values_train, MAE_values_test=MAE_values_test,
+                  num_iter=num_iter, alpha=alpha)
+
+    return theta_list
 
 if __name__ == '__main__':
     num_iter = 500
-    alpha = 0.001
-    train_test_proportion = 0.8
-    theta_list, MSE_x_train, MSE_x_test = run(num_iter=num_iter, alpha=alpha, train_test_proportion=train_test_proportion)
+    alpha = 0.01
+    train_test_proportion = 0.9
+    theta_list = run(num_iter=num_iter, alpha=alpha, train_test_proportion=train_test_proportion)
     print('Theta_list: \n', theta_list)
-    print('MSE_train: ', MSE_x_train)
-    print('MSE_test: ', MSE_x_test)
